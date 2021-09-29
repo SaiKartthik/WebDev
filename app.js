279bd13349cba46aabf1f7865c0de618b122baad
Sai Karthik <84378022+SaiKartthik@users.noreply.github.com>
2021-09-29 19:18:42 +0530

    Query selectors

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Hello")
+var heading = document.getElementById('heading')
+var para = document.getElementsByClassName('para')
+var tags = document.getElementsByTagName('a')
+var query = document.querySelector('.para')
+console.log(heading);
+console.log(para);
+console.log("Done");
+console.log(query)
