1ba15c6b63311cb6d47641e4f44667e16544e304
Sai Karthik <84378022+SaiKartthik@users.noreply.github.com>
2021-10-01 13:17:26 +0530

    Functions

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-var list = document.querySelector('ul')
-
-
-var arr = ['Go the GYM ', 'cook oatmeal', 'Eat']
-
-arr.push('Complete the assignment')
-
-for (var i = 0; i < arr.length; i++) {
-    var element = document.createElement('li')
-    var textNode = document.createTextNode(arr[i])
-    element.appendChild(textNode)
-    list.appendChild(element)
+function greet(name)
+{
+  console.log("hello world",name)
 }
+greet("SaiKarthik")
+console.log("Done");
