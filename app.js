1273715d647e887334f9540c34add0fc946ce192
Sai Karthik <84378022+SaiKartthik@users.noreply.github.com>
2021-10-01 14:13:27 +0530

    buttons

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-function greet(a,b)
-{
-  console.log("hello world",a)
-  return a+b
+var button = document.querySelector("button")
+var input = document.querySelector("input")
+var list = document.querySelector("ul")
+console.log(button,input,list)
+
+const callbackfunc = (event) => {
+  const inputval = input.value
+  const element  = document.createElement("li")
+  const textnode = document.createTextNode(inputval)
+  element.appendChild(textnode)
+  list.appendChild(element)
+
 }
-console.log(greet(1,2))
-console.log("Done");
+
+button.addEventListener("click",callbackfunc)
