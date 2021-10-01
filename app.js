8861d8f25d754d551b73bb2c2e4b350e33fa33e7
Sai Karthik <84378022+SaiKartthik@users.noreply.github.com>
2021-10-01 14:18:56 +0530

    toggle dark mode

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 var button = document.querySelector("button")
-var input = document.querySelector("input")
-var list = document.querySelector("ul")
-console.log(button,input,list)
+var body = document.querySelector("body")
 
 const callbackfunc = (event) => {
-  const inputval = input.value
-  const element  = document.createElement("li")
-  const textnode = document.createTextNode(inputval)
-  element.appendChild(textnode)
-  list.appendChild(element)
+  body.classList.toggle("dark")
 
 }
 
