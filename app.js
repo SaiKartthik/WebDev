db4c2e75c2a4a6ac6be5406232d4acef7e4210cb
Sai Karthik <84378022+SaiKartthik@users.noreply.github.com>
2021-10-10 22:21:51 +0530

    node require

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-//Js async await
-
-var attend = false;
-
-const test = new Promise((resolve,reject) =>
-{
-  setTimeout(function() {
-  if(attend) resolve("we attended the concert")
-  else reject("we failed to attend")
-}, 1000);
-});
-
-// test.then((data) => console.log(data)).catch((data) => console.log(data))
-
-const getconcertresult = async () => {
-  try{
-    const getresult = await test;
-    console.log(getresult);}
-  catch(error){
-  console.log(error);
-}};
-getconcertresult();
+console.log(require('./index'))
+console.log(require('./util'))
+const {add,sub} = require('./util')
+add()
---
+++ util.js
@@ -0,0 +1,9 @@
+const add = () => {
+  console.log("I add elements");
+}
+const sub = () => {
+  console.log("I sub elements");
+}
+module.exports.add = add
+module.exports.sub = sub
+console.log(module);
