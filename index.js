0604cc9ee88817305c43110883a3fd5394b55f7a
Sai Karthik <84378022+SaiKartthik@users.noreply.github.com>
2021-10-16 16:36:09 +0530

    student filtering

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ deadpool.get("/b2/:id",(req,res) =>
 ]
   studId = req.params.id
   getData = studs.filter((i) => parseInt(studId) === i.id)
+  if(getData.length == 0 ) return res.json("No student found")
+
   return res.json(getData)
 })
 
