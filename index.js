cfdfcf84cbd006d84c12e0eac756fe9625e6aee2
Sai Karthik <84378022+SaiKartthik@users.noreply.github.com>
2021-10-16 16:31:15 +0530

    server filter

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ const express = require("express")
 
 const deadpool = express()
 
+deadpool.use(express.json())
+
 deadpool.get("/",(request,response) => {
   return response.json("Bye World")
 })
 
+deadpool.get("/b2/:id",(req,res) =>
+{
+  studs = [{
+    id : 1 ,
+    name : "Sai"
+  },
+  {
+    id : 2 ,
+    name : "Karthik"
+  },
+  {
+    id : 3 ,
+    name : "M"
+  }
+]
+  studId = req.params.id
+  getData = studs.filter((i) => parseInt(studId) === i.id)
+  return res.json(getData)
+})
+
 deadpool.listen(5000,() => console.log("Getting started 🚀"))
+
+
+//
