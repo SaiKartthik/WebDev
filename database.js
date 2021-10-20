8bd1043d60a3fd19090f9e40326ebd4af2e9d700
Sai Karthik <84378022+SaiKartthik@users.noreply.github.com>
2021-10-20 20:15:13 +0530

    using bookapi's

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ const books = [{
   numPage : 250 ,
   author : [1,2] ,
   publications : [1] ,
-  category : ["Nothing", "Testing"]
+  category : ["Nothing", "Testing","tech"]
 
 }
 ]
--- index.js
+++ index.js
@@ -4,12 +4,120 @@ const database = require("./database")
 
 const booky = express();
 
+/*
+Route               /
+Description         Get book with ISBN
+Access              Public
+Parameter           None
+Methods             Get
+*/
+
 booky.get("/",(req, res) => {
   return res.json({books: database.books})
 
 })
 
-booky.listen(3000,() => console.log("Server is running👀"))
+/*
+Route               /
+Description         Get book with ISBN
+Access              Public
+Parameter           isbn
+Methods             Get
+*/
+
+booky.get("/is/:isbm", (req, res) => {
+  var bookID = req.params.isbm
+  const getspecificbook = database.books.filter((book) => book.ISBN == bookID)
+  if(getspecificbook.length == 0 )
+  {
+      return res.json(`No book found with ISBN of ${(bookID)}`)
+
+  }
+  else
+  {
+    return res.json(getspecificbook)
+  }
+})
+
+/*
+Route               /
+Description         Get book based on category
+Access              Public
+Parameter           category
+Methods             Get
+*/
+
+booky.get("/c/:category",(req, res) => {
+  var bookcat = req.params.category
+  const getspecificbook = database.books.filter((book) => book.category.includes(bookcat) )
+  if(getspecificbook.length == 0 )
+  {
+      return res.json(`No book found in category ${(bookcat)} `)
+
+  }
+  else
+  {
+    return res.json(getspecificbook)
+  }
+} )
+
+/*
+Route               /l
+Description         Get book based on category
+Access              Public
+Parameter           lan
+Methods             Get
+*/
+
+booky.get("/l/:lan",(req, res) => {
+  var booklan = req.params.lan
+  const getspecificbook = database.books.filter((book) => book.language.includes(booklan) )
+  if(getspecificbook.length == 0 )
+  {
+      return res.json(`No book found in  ${(booklan)} language `)
+
+  }
+  else
+  {
+    return res.json(getspecificbook)
+  }
+} )
+
+/*
+Route               /author
+Description         Get all authors
+Access              Public
+Parameter           None
+Methods             Get
+*/
+
+booky.get("/author", (req, res) => {
+    return res.json(database.author)
+})
+
+/*
+Route               /author/book
+Description         Get all authors based on books
+Access              Public
+Parameter           None
+Methods             Get
+*/
+
+booky.get("/author/book/:isbn", (req, res) =>
+{
+  var bookisbn = req.params.isbn
+  const getspecificbook = database.author.filter((author) => author.books.includes(bookisbn))
+  if(getspecificbook.length == 0)
+  {
+      return res.json(error)
+
+  }
+  else{
+  return res.json(getspecificbook)
+}
+})
+
+booky.listen(3000,() => console.log("Server is running 👀"))
 
 
 //
