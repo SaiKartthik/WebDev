69d77e0aa8f8268947c4b9a1ead6e195ec4f9c42
SaiKartthik <saikarthikmedepalli>
2022-02-26 13:27:37 +0530

    Creating table to display users

--- app.js
+++ app.js
@@ -5,17 +5,23 @@ var body = document.querySelector("body");
 var head = document.querySelector("h1");
 var lst = document.querySelector("ul");
 var ill = ["Dey", "Hads", "hfsguyg"];
+var table = document.querySelector("table");
 
 var x = fetch("https://jsonplaceholder.typicode.com/users")
-  .then((res) => res.json())
+  .then((req) => req.json())
   .then((data) => {
-    var appenddata = (datareq) => {
-        var ele = datareq['name'] + " " + datareq['username'] + datareq['email']
-        var element = document.createElement("li");
-        var textNode = document.createTextNode(ele);
-        element.appendChild(textNode);
-        lst.appendChild(element);
+    
+    for (user of data) {
+      let newRow = document.createElement("tr");
+      var templ = `
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        <td>${user.username}</td>
+        <td>${user.email}</td>
+      `
+      newRow.innerHTML = templ
+      table.appendChild(newRow);
+      
     }
-    data.forEach(appenddata)
-    console.log(data);
+    
   });
