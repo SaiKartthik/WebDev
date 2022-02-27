0c95cc45e1fd3a59df1f703acdee1f179e82a922
SaiKartthik <saikarthikmedepalli>
2022-02-27 22:44:25 +0530

    http requests

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-fs = require('fs')
-fs.readFile('./file.txt','utf8',(error, data) => {
-    if(error){
-        console.log(error);
-    }
-    else{
-        console.log(data);
-    }
-})
+console.log("Hello world");
 
-console.log(fs.readFileSync('./file.txt', 'utf8'))
+const http = require("http");
 
-fs.writeFileSync('./file.txt',"Hello World")
+const server = http.createServer((request, response) => {
+  console.log(request.url);
+  response.write("Bye world");
+  response.end();
+});
+
+server.listen(3000, function () {
+  console.log("server is running");
+});
