c9cf3daf60f978eb957f70b01bd67becb414edda
SaiKartthik <saikarthikmedepalli>
2022-02-27 23:29:15 +0530

    commit

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 console.log("Hello world");
 
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
+x = fs.readFileSync('./', "utf8");
 const server = http.createServer((request, response) => {
   console.log(request.url);
-  response.write("Bye world");
+  response.write(`<h1>Bye World</h1>`);
   response.end();
 });
 
