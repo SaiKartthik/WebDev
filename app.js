4b28339ed9fc5f6923056e8293c71b6e2371a0a6
SaiKartthik <saikarthikmedepalli>
2022-03-01 23:01:03 +0530

    getting started with Api

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+const names = [
+  {Name : "Sai"},
+  {Name : "Karthik"},
+
+]
+
+const express = require("express");
+const app = express();
+
+app.get('/getnames', (req, res) => {
+  res.send(names)
+})
+app.get("", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, function () {
+  console.log("server is running");
+});
+
 // console.log("Hello world");
 
 // const http = require("http");
@@ -14,14 +34,3 @@
 // server.listen(3000, function () {
 //   console.log("server is running");
 // });
-
-const express = require("express");
-const app = express();
-
-app.get("", (req, res) => {
-  res.send("Hello World")
-})
-
-app.listen(3000, function () {
-  console.log("server is running");
-})
