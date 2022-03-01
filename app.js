b65c808ed68440a3ab9125787b1435299b0d10b0
SaiKartthik <saikarthikmedepalli>
2022-03-01 22:44:44 +0530

    Creating server with React

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-console.log("Hello world");
+// console.log("Hello world");
 
-const http = require("http");
-const path = require("path");
-const fs = require("fs");
+// const http = require("http");
+// const path = require("path");
+// const fs = require("fs");
 
-x = fs.readFileSync('./index.html');
-const server = http.createServer((request, response) => {
-  console.log(request.url);
-  response.write(x);
-  response.end();
-});
+// x = fs.readFileSync('./index.html');
+// const server = http.createServer((request, response) => {
+//   console.log(request.url);
+//   response.write(x);
+//   response.end();
+// });
 
-server.listen(3000, function () {
+// server.listen(3000, function () {
+//   console.log("server is running");
+// });
+
+const express = require("express");
+const app = express();
+
+app.get("", (req, res) => {
+  res.send("Hello World")
+})
+
+app.listen(3000, function () {
   console.log("server is running");
-});
+})
