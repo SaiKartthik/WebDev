792ab7508330b7e51ef7acb4ee0636f5f12ca882
SaiKartthik <saikarthikmedepalli>
2022-03-06 15:38:30 +0530

    adding and displaying prod

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-const names = [
-  {Name : "Sai"},
-  {Name : "Karthik"},
-
-]
-
 const express = require("express");
-const app = express();
+app = express();
+PORT = 3000;
+app.use(express.json());
+products = [
+  { name: "iphone12", price: "$1099" },
+  { name: "iphone13", price: "$1199" },
+  { name: "iphone14", price: "$1299" },
+];
 
-app.get('/public', (req, res) => {
-  res.send("This is Public");
-})
+app.post("/products/add", (req, res) => {
+  console.log(req.body);
+  const{name, price} = req.body;
+  if(!name || !price){
+    res.send("Validation Failed")
+  }
+  else{
+  
+  products.push({name, price});
+  res.send(products);
+}});
 
-isAdmin = (req, res, next) => {
-  if(req.headers.admin === 'true') next();
-  else res.send("You are not authorised")
-}
-app.get('/private',isAdmin, (req, res) => {
-  res.send("Welcome to Admin routes");
-})
 
-// app.post('/signup', (req, res) => {
-//   console.log(req.body);
-//   res.send("Req sent")
-// })
-app.listen(3000, function () {
-  console.log("server is running");
+app.get("/products", (req, res) => {
+  res.json(products);
 });
 
+app.listen(PORT, () => console.log("Yay server is running"));
